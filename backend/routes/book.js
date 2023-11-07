c5a3708dd1e396d0c088d4ab8c6a4f913847d39f
Poussin-5 <gpautet@hotmail.fr>
2023-11-07 09:11:11 +0100

    début backend avec ajout connexion et login ainsi que les routes api

---
+++ backend/routes/book.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const bookCtrl = require('../controllers/book');
+
+router.get('/', bookCtrl.getAllBooks );
+router.get('/:id', bookCtrl.getOneBook);
+ router.get('/bestrating', bookCtrl.getBestBooks);
+router.post('/',auth, multer ,bookCtrl.createBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook );
+router.delete('/:id',auth, bookCtrl.deleteBook );
+router.post('/:id/rating', auth, bookCtrl.rateBook)
+
+module.exports = router;
+
