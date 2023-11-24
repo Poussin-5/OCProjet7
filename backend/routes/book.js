976d2371ed5cf8aeaaec4c398e5e9307b4565feb
Poussin-5 <gpautet@hotmail.fr>
2023-11-24 08:58:46 +0100

    eslint

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 
+const bookCtrl = require('../controllers/book')
 
-const bookCtrl = require('../controllers/book');
-
-
-router.get('/', bookCtrl.getAllBooks );
-router.get('/bestrating', bookCtrl.getBestBooks);
-router.get('/:id', bookCtrl.getOneBook);
-router.post('/',auth, multer, bookCtrl.createBook);
-router.put('/:id', auth,  multer, bookCtrl.modifyBook );
-router.delete('/:id',auth, bookCtrl.deleteBook );
+router.get('/', bookCtrl.getAllBooks)
+router.get('/bestrating', bookCtrl.getBestBooks)
+router.get('/:id', bookCtrl.getOneBook)
+router.post('/', auth, multer, bookCtrl.createBook)
+router.put('/:id', auth, multer, bookCtrl.modifyBook)
+router.delete('/:id', auth, bookCtrl.deleteBook)
 router.post('/:id/rating', auth, bookCtrl.rateBook)
 
-module.exports = router;
-
+module.exports = router
